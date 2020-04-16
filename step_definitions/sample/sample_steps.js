8830e3c32ace4b3e335fd90adedf9e1becc3ae54
izhanh <35693359+izhanh@users.noreply.github.com>
2020-04-16 17:14:24 +0200

    Update sample_steps.js

--- step_definitions/sample/sample_steps.js
+++ step_definitions/sample/sample_steps.js
@@ -1,24 +1,95 @@
 /*
-  [SAMPLE] SAMPLE TEST STEPS
+  [SAMPLE] TEST FUNCTIONS
 
-  Author : Izhan Hernández
-  Date Created : April 1st 2020
+  Author: Izhan Hernández
+  Date Created: April 1st 2019
 */
 
-// IMPORTS
+// IMPORT
 
-const { I, sample_sample_test } = inject();
+const { I } = inject();
 
-// GHERKIN SENTENCES
+// CONSTANTS
 
-Given('user opens Google page', () => {
-  sample_sample_test.getURL();
-});
+const sampleUrl = "https://www.google.com";
 
-Given('user passes the test', () => {
-  sample_sample_test.smaplePass();
-});
+// TESTS
 
-Given('user fails the test', () => {
-  sample_sample_test.smapleFail();
-});
+module.exports = {
+  // FUNCTIONS
+
+  getURL() {
+    I.amOnPage(sampleUrl);
+  },
+
+  // Sample Pass
+  smaplePass() {
+    I.assert("1", "1");
+  },
+
+  // Sample Pass
+  smapleFail() {
+    I.assert("1", "2");
+  },
+
+  // Test setting a config param
+  async sampleSetConfigParamTest() {
+    I.say("===> Setting config param [random] = 'value'");
+    await I.setConfigParamenter("random", "value");
+  },
+
+  // Test getting a config param
+  async sampleGetConfigParamTest() {
+    I.say(`===> Config param [env] = ${await I.getConfigParamenter("env")}`);
+    I.say(
+      `===> Config param [random] = ${await I.getConfigParamenter("random")}`
+    );
+  },
+
+  // Test getting a config param
+  async sampleSetGlobalVarTest() {
+    I.say("===> Setting Global Variable [random] = 'value'");
+    await I.setGlobalVar("random", "value");
+    await I.setGlobalVar("random2", "value2");
+  },
+
+  // Test getting a config param
+  async sampleGetGlobalVarTest() {
+    I.say(`===> Global Variable [random] = ${await I.getGlobalVar("random")}`);
+  },
+
+  // Test generating a mail inbox
+  async sampleMailTest() {
+    // Generate a mailbox
+    const mailBox = await I.haveNewMailbox();
+    I.setCurrentMailInbox(mailBox);
+    I.say(`New mailbox created: ${await I.getCurrentMailInbox()}`);
+
+    // Send a mail to the recently created inbox
+    I.sendEmail({ to: [mailBox.emailAddress], subject: "Test", body: "Email" });
+
+    // Check that the email was received
+    I.waitForLatestEmail();
+    // Assert we received the mail
+    I.seeInEmailSubject("Test");
+  },
+
+  getAllBrowserMethods() {
+    I.getALlDriverMethods();
+  },
+
+  captureLogsTest() {
+    I.say(I.grabBrowserLogs());
+    //I.saveBrowserLogs();
+  },
+
+  changeScreenSizeTest(width, height) {
+    I.say(`Changing screen size to: [${width}x${height}]`);
+    I.changeScreenSizeTo(width, height);
+  },
+
+  changeScreenSizeDeviceTest(device) {
+    I.say(`Changing screen size to: [${device}]`);
+    I.changeScreenSizeToDevice(device);
+  }
+};
